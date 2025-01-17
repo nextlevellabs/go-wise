6ac62a3843d5f10d8c97e24f5f2f27576bce27df
Eduardo Marcelino Jr <eduardojunior.2000@alunos.utfpr.edu.br>
2025-01-17 17:57:25 -0300

    feat: export options

---
+++ wise/pkg/mongodb/options.go
@@ -0,0 +1,18 @@
+package mongodb
+
+import (
+	"github.com/nextlevellabs/go-wise/wise/internal/filters"
+	"github.com/nextlevellabs/go-wise/wise/internal/mongodb"
+)
+
+func WithBloomFilter(bf filters.Bloom[string]) mongodb.RepositoryOptions {
+	return mongodb.WithBloomFilter(bf)
+}
+
+func WithMaxPageSize(size int) mongodb.RepositoryOptions {
+	return mongodb.WithMaxPageSize(size)
+}
+
+func WithDefaultPageSize(size int) mongodb.RepositoryOptions {
+	return mongodb.WithDefaultPageSize(size)
+}
